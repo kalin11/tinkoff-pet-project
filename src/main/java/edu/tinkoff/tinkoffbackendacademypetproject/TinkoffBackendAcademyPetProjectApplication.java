85e404bef6dd49d37a220e97c7af125601e94ffb
Artem Lysenko <artemlysenko@niuitmo.ru>
2023-11-19 01:56:50 +0300

    feat: add properties files and docker files

---
+++ src/main/java/edu/tinkoff/tinkoffbackendacademypetproject/TinkoffBackendAcademyPetProjectApplication.java
@@ -0,0 +1,13 @@
+package edu.tinkoff.tinkoffbackendacademypetproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinkoffBackendAcademyPetProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TinkoffBackendAcademyPetProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/tinkoff/tinkoffbackendacademypetproject/TinkoffBackendAcademyPetProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.tinkoff.tinkoffbackendacademypetproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinkoffBackendAcademyPetProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
