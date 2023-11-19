1a3748c9d8db156ab1e4cb21bfb43ae58d285c7f
Daniil <Daniil141980@yandex.ru>
2023-11-19 19:49:08 +0300

    feat: add method to find subject topic by id

--- src/main/java/edu/tinkoff/tinkoffbackendacademypetproject/services/SubjectTopicService.java
+++ src/main/java/edu/tinkoff/tinkoffbackendacademypetproject/services/SubjectTopicService.java
@@ -1,7 +1,9 @@
 package edu.tinkoff.tinkoffbackendacademypetproject.services;
 
+import edu.tinkoff.tinkoffbackendacademypetproject.exceptions.EntityModelNotFoundException;
 import edu.tinkoff.tinkoffbackendacademypetproject.model.SubjectTopic;
 import edu.tinkoff.tinkoffbackendacademypetproject.repositories.SubjectTopicCriteriaRepository;
+import edu.tinkoff.tinkoffbackendacademypetproject.repositories.SubjectTopicRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,15 +20,21 @@ public class SubjectTopicService {
      * Репозиторий для работы с топиками для предметов
      */
     private final SubjectTopicCriteriaRepository criteriaRepository;
+    private final SubjectTopicRepository subjectTopicRepository;
 
     /**
      * Получение всех топиков по заданному курсу и названию
+     *
      * @param courseNumber номер курса
-     * @param name название предмета
+     * @param name         название предмета
      * @return список всех топиков
      */
     @Transactional
     public List<SubjectTopic> findAllByCourseNumberAndSubjectName(Long courseNumber, String name) {
         return criteriaRepository.findAllByCourseNumberAndSubjectName(courseNumber, name.toUpperCase());
+    }
+
+    public SubjectTopic getSubjectTopic(Long id) throws EntityModelNotFoundException {
+        return subjectTopicRepository.findById(id).orElseThrow(() -> new EntityModelNotFoundException("Топика", "id", id));
     }
 }
