1c80861a98709da14eb08ee2c474c4857ed34cd6
Dmitrii Art <work.dmitriiart@gmail.com>
2023-11-21 11:38:28 +0000

    [Feature]: Cors for frontend development

--- src/main/java/edu/tinkoff/tinkoffbackendacademypetproject/TinkoffBackendAcademyPetProjectApplication.java
+++ src/main/java/edu/tinkoff/tinkoffbackendacademypetproject/TinkoffBackendAcademyPetProjectApplication.java
@@ -2,8 +2,13 @@ package edu.tinkoff.tinkoffbackendacademypetproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+@EnableConfigurationProperties
 public class TinkoffBackendAcademyPetProjectApplication {
 
     public static void main(String[] args) {
